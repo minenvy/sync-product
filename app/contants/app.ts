2191b25070cd418cf40533ced4e02328f5c4b6fd
Jethro <minhnv131001@gmail.com>
2025-10-08 23:24:20 +0700

    Add constants

---
+++ app/contants/app.ts
@@ -0,0 +1,7 @@
+export const actions = {
+  getProductDetail: "getProductDetail",
+  syncProduct: "syncProduct",
+};
+
+export const startKeyToGetVariant = "additionalImages";
+export const endKeyToGetVariant = "itemCreationDate";
---
+++ app/contants/ebay.ts
@@ -0,0 +1,1 @@
+export const TOKEN_EXPIRED_TIME = 7200; // seconds
